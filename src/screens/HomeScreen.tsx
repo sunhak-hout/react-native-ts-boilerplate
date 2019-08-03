847ab8e6f10ad999de9fd6d3fa994cb55c032ccc
sunhakhout <sunhak.hout@gmail.com>
2019-08-03 23:38:56 +0700

    feat: babel module resolver

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const HomeScreen: React.FC = () => {
+  return <Text>Hello From HomeScreen</Text>;
+};
+
+export default HomeScreen;
